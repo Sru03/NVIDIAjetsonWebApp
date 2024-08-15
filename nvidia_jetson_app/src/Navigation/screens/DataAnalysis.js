fe3f1858f6cf17606719e7bb26beaf32967d1ec2
Srushti M.B <mbsrushti@gmail.com>
2024-08-15 19:06:59 +1200

    latest update

--- nvidia_jetson_app/src/Navigation/screens/DataAnalysis.js
+++ nvidia_jetson_app/src/Navigation/screens/DataAnalysis.js
@@ -1,7 +1,62 @@
 import React from "react" ;
+import './DataAnalysis.css';
 
-const DataAnalysis = () => {
-    return <h1> DataAnalysis </h1>;
+const data = [ 
+    {name: "Anom", position: "Standing", gender: "Male", date: "12-08-2024:7:10:45", coordinates: " (50,50)"},
+    {name: "Sana", position: "Sitting", gender: "Female", date: "12-08-2024:7:10:45",coordinates: "(10,29)"},
+    {name: "Rohit", position: "Sleeping", gender: "Male", date: "12-08-2024:7:10:45",coordinates: "(30,40)"},
+    {name: "Aman", position: "Standing", gender: "Male", date: "12-08-2024:7:10:45",coordinates: "(12,19)"},
+
+]
+
+
+
+function DataAnalysis() {
+
+    return (
+
+        <div className="main">
+            <div> <h1> DataAnalysis</h1></div>
+
+            <div className ="DataTable"><table>
+                <tr>
+                    <th>Name </th>
+                    <th>Position </th>
+                    <th>Gender </th>
+                    <th>Date </th>
+                    <th>Co-ordinates</th>
+                   </tr>
+
+                   {data.map((val, key) => {
+                    return(
+                        <tr key = {key}>
+                            <td>{val.name}</td>
+                            <td>{val.position}</td>
+                            <td>{val.gender}</td>
+                            <td>{val.date}</td>
+                            <td>{val.coordinates}</td>
+
+                        </tr>
+                    )
+                   })}
+               
+                
+            </table></div>
+
+
+            <div className ="PieChart"> 
+             <h1> PieChart</h1>
+            </div>
+
+
+
+
+        </div>
+    
+    );
+
 }
+
+
 
 export default DataAnalysis; 
--- nvidia_jetson_app/src/Navigation/screens/Display.js
+++ nvidia_jetson_app/src/Navigation/screens/Display.js
@@ -1,5 +1,9 @@
 import React from "react";
 import './Display.css';
+import { Link } from 'react-router-dom';
+
+
+
 
 
 function Display() {
@@ -12,25 +16,37 @@ function Display() {
 
             <div className=" containers">
                 <div className="displayContainer">
-                  <h1> displayContainer </h1>
+
+                    <h1> displayContainer </h1>
+
                 </div>
-                 <div className= "subContainer">
-                 <div className="videoStreamingContainer">
-                    <h1> VideoStreamingContainer</h1>
+                <div className="subContainer">
+
+                    <div className="videoStreamingContainer">
+
+                        <h1> VideoStreamingContainer</h1>
+                        
+                    </div>
+
+                    <div className="mapContainer">
+                        <h1> MapConatainer</h1>
+                    </div>
+                </div>
+                <div className="iconContainer">
+
+
+                    <div className="icon_one"><h1> icon1</h1></div>
+
+                    <div className="icon_two"><h1> icon2</h1>
+                        <nav>
+                            <Link to="/DataAnalysis"> icon2 </Link>
+                        </nav>
+
+                    </div>
+
+
                 </div>
 
-                <div className="mapContainer">
-                  <h1> MapConatainer</h1>
-                </div>
-                 </div>
-                 <div className =" iconContainer ">
-                    <div><h1> icon1</h1></div>
-
-                    <div><h1> icon2</h1></div>
-
-
-                 </div>
-                
 
             </div>
         </div>
